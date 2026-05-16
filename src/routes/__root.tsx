8d4530d3d2ba08e00fc274813ee7a9961b65bdf8
Abdul Rawoof <i.rawoof47@gmail.com>
2026-05-16 18:00:31 +0530

    adad

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -80,7 +80,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
       meta: [
         { charSet: "utf-8" },
         { name: "viewport", content: "width=device-width, initial-scale=1" },
-        { title: "Rawoof" },
+        { title: "Propel" },
         {
           name: "description",
           content:
